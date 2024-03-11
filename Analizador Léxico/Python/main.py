856740f94ce918009b9853ae7f8d10627edf8da3
Alejandro Vargas <61713783+AlejandroV074@users.noreply.github.com>
2024-03-10 23:39:14 -0500

    Feature/txt read (#3)
    * se agrega archivo de texto
    
    * feature: Lee archivo de texto implementa keyword

--- Analizador Léxico/Python/main.py
+++ Analizador Léxico/Python/main.py
@@ -1,44 +1,25 @@
 from ply import lex
+import keyword
 
 tokens = (
     'IDENTIFICADOR',
     'NUMERO',
     'OPERADOR',
     'SIMBOLO_ESPECIAL',
-    'PALABRA_RESERVADA',
     'CADENA',
 )
 
 t_NUMERO = r'\d+'
 t_OPERADOR = r'[+\-*/=]'
-t_SIMBOLO_ESPECIAL = r'[(),:]'
+t_SIMBOLO_ESPECIAL = r'[(),:\[\]]'
 t_CADENA = r'\"[^\"]*\"'
-
-t_ignore = ' \t'
+t_ignore = ' \n\r'
 
 def t_error(t):
     print("Carácter ilegal '%s'" % t.value[0])
     t.lexer.skip(1)
 
-reserved = {
-    'print': 'PALABRA_RESERVADA',
-    'if': 'PALABRA_RESERVADA',
-    'else': 'PALABRA_RESERVADA',
-    'while': 'PALABRA_RESERVADA',
-    'for': 'PALABRA_RESERVADA',
-    'class': 'PALABRA_RESERVADA',
-    'int': 'PALABRA_RESERVADA',
-    'try': 'PALABRA_RESERVADA',
-    'except': 'PALABRA_RESERVADA',
-    'None': 'PALABRA_RESERVADA',
-    'and': 'PALABRA_RESERVADA',
-    'or': 'PALABRA_RESERVADA',
-    'import': 'PALABRA_RESERVADA',
-    'as': 'PALABRA_RESERVADA',
-    'def': 'PALABRA_RESERVADA',
-    'return': 'PALABRA_RESERVADA',
-}
-
+reserved = {word: 'PALABRA_RESERVADA' for word in keyword.kwlist}
 tokens += tuple(reserved.values())
 
 def t_IDENTIFICADOR(t):
@@ -58,24 +39,15 @@ def analizador_lexico(expression):
     return analyzed_tokens
 
 def main():
-    print("Bienvenido al Analizador Léxico")
-    while True:
-        print("\nMenú:")
-        print("1. Digitar texto")
-        print("2. Salir")
-        opcion = input("Seleccione una opción: ")
-
-        if opcion == "1":
-            texto = input("Por favor, ingrese el texto a analizar: ")
+    archivo = "texto.txt"  
+    try:
+        with open(archivo, "r") as file:
+            texto = file.read()
             tokens = analizador_lexico(texto)
-            print("\nSeparación de tokens:")
             for token_type, token_value in tokens:
                 print(f"{token_type}: {token_value}")
-        elif opcion == "2":
-            print("¡Hasta luego!")
-            break
-        else:
-            print("Opción inválida. Por favor, seleccione una opción válida.")
+    except FileNotFoundError:
+        print("Archivo no encontrado. Por favor, asegúrese de que exista un archivo llamado 'texto.txt'.")
 
 if __name__ == "__main__":
     main()
